b9f2a54b3ed09c30d509c61a2ca3ae5f529cdf92
Lyrae <1078069438@qq.com>
2021-06-04 11:52:39 +0800

    xc_0604_1151
    Add XcForm

---
+++ GameCollectionApp/GameCollectionApp/xc/XcForm.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCollectionApp.xc
+{
+  public partial class XcForm : Form
+  {
+    public XcForm()
+    {
+      InitializeComponent();
+    }
+
+    private void XcForm_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
