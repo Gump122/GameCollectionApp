b8cecc935945083cbaaaf5d70dca065b6100e639
沈为 <1048737191@qq.com>
2021-06-04 14:39:26 +0800

    sw_0604_1440

--- GameCollectionApp/GameCollectionApp/MainForm.Designer.cs
+++ GameCollectionApp/GameCollectionApp/MainForm.Designer.cs
@@ -31,14 +31,15 @@ namespace GameCollectionApp
             this.dkButton = new System.Windows.Forms.Button();
             this.ljwButton = new System.Windows.Forms.Button();
             this.xcButton = new System.Windows.Forms.Button();
+            this.swButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // dkButton
             // 
-            this.dkButton.Location = new System.Drawing.Point(57, 48);
-            this.dkButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dkButton.Location = new System.Drawing.Point(51, 40);
+            this.dkButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.dkButton.Name = "dkButton";
-            this.dkButton.Size = new System.Drawing.Size(112, 34);
+            this.dkButton.Size = new System.Drawing.Size(100, 28);
             this.dkButton.TabIndex = 0;
             this.dkButton.Text = "dk";
             this.dkButton.UseVisualStyleBackColor = true;
@@ -46,10 +47,10 @@ namespace GameCollectionApp
             // 
             // ljwButton
             // 
-            this.ljwButton.Location = new System.Drawing.Point(238, 46);
-            this.ljwButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ljwButton.Location = new System.Drawing.Point(212, 38);
+            this.ljwButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.ljwButton.Name = "ljwButton";
-            this.ljwButton.Size = new System.Drawing.Size(112, 34);
+            this.ljwButton.Size = new System.Drawing.Size(100, 28);
             this.ljwButton.TabIndex = 1;
             this.ljwButton.Text = "ljw";
             this.ljwButton.UseVisualStyleBackColor = true;
@@ -57,23 +58,36 @@ namespace GameCollectionApp
             // 
             // xcButton
             // 
-            this.xcButton.Location = new System.Drawing.Point(435, 48);
+            this.xcButton.Location = new System.Drawing.Point(387, 40);
+            this.xcButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.xcButton.Name = "xcButton";
-            this.xcButton.Size = new System.Drawing.Size(99, 32);
+            this.xcButton.Size = new System.Drawing.Size(88, 27);
             this.xcButton.TabIndex = 2;
             this.xcButton.Text = "xc";
             this.xcButton.UseVisualStyleBackColor = true;
             this.xcButton.Click += new System.EventHandler(this.xcButton_Click);
             // 
+            // swButton
+            // 
+            this.swButton.Location = new System.Drawing.Point(549, 38);
+            this.swButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.swButton.Name = "swButton";
+            this.swButton.Size = new System.Drawing.Size(100, 28);
+            this.swButton.TabIndex = 3;
+            this.swButton.Text = "sw";
+            this.swButton.UseVisualStyleBackColor = true;
+            this.swButton.Click += new System.EventHandler(this.swButton_Click);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 675);
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.swButton);
             this.Controls.Add(this.xcButton);
             this.Controls.Add(this.ljwButton);
             this.Controls.Add(this.dkButton);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -86,6 +100,7 @@ namespace GameCollectionApp
 
         private System.Windows.Forms.Button dkButton;
         private System.Windows.Forms.Button ljwButton;
-    private System.Windows.Forms.Button xcButton;
-  }
+        private System.Windows.Forms.Button xcButton;
+        private System.Windows.Forms.Button swButton;
+    }
 }
---
+++ GameCollectionApp/GameCollectionApp/sw/swForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace GameCollectionApp.sw
+{
+    partial class SwForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(43, 38);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.Text = "exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Name = "swForm";
+            this.Text = "swForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+    }
+}
